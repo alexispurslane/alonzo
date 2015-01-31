b2d701182ab63629c4a444c3b2338a42e0bf1ea1
christopherdumas <christopherdumas@me.com>
2015-01-31 08:59:33 -0800

    Lists, clojures, scoping all finished

--- alonzo.rkt
+++ alonzo.rkt
@@ -4,11 +4,13 @@
 (define-namespace-anchor anc)
 (define ns (namespace-anchor->namespace anc))
 (define area '())
-(define (lookup key list)
-  (define result (assoc key list))
+(define (lookup key list block-level (rep #f))
+  (define result (assoc key
+                        (filter (lambda (e)
+                                  (or (<= (third e) block-level) rep)) list)))
   (cond
-    [(not result) (displayln (string-append "Cannot locate variable for " (symbol->string key)))]
-    [(eq? (car (cdr result)) key) (lookup key (cdr list))]
+    [(not result) (displayln (string-append "Cannot locate variable for " (symbol->string key) ", from block level: " (number->string block-level)))]
+    [(eq? (second result) key) (lookup key (cdr list) block-level #t)]
     [else result]))
 
 (define (balanced? lst)
@@ -55,54 +57,58 @@
 (define (convert-bool bool)
   (if bool (exec '(true)) (exec '(false))))
 
+(define (parenify list (level 0))
+  (cond
+    [(null? list) '()]
+    [(= level 0)
+     (cons (car list) (parenify (cdr list) (+ level 1)))]
+    [else `(,(car list) ,(parenify (cdr list)))]))
+
+(define (slist->string slst)
+  (string-join (map symbol->string slst) " "))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
-(define (exec code [in-block #f])
+(define (exec code [block-level 0])
   (define res (match code
                 [(list 'begin thing ... 'end)   (car (cdr (map exec thing)))]
                 [`(λ ,arg ,body)    (list `(,arg) `,body 'func)]
                 [`(fn ,args ,body)  (list `,args `,body 'func)]
+                [`(l ,lst) (exec (parenify (add-between (flatten (cons lst 'empty-list)) ':)) block-level)]
                 
                 [`(let ,assocs ,body) (for ([i assocs])
-                                        (set! area (cons (list i #t) area)))
-                                      (exec body #t)]
+                                        (set! area (cons (list i (+ block-level 1)) area)))
+                                      (exec body (+ block-level 1))]
                 
-                [`(,thing ,args) (define fn (exec thing))
+                [`(,thing ,args) (define fn (exec thing block-level))
                                  (for ([v args]
                                        [k (car fn)])
-                                   (set! area (cons (list k v #t) area)))
-                                 (map (lambda (e) (exec e #t)) (car (cdr fn)))]
+                                   (set! area (cons (list k v (+ block-level 1)) area)))
+                                 (last (map (lambda (e)
+                                              (display e)
+                                              (displayln (string-append " at " (number->string (+ block-level 1))))
+                                              (exec e (+ block-level 1))) (second fn)))]
                 
                 [`(,a + ,b)            (+ (exec a) (exec b))]
                 [`(,a - ,b)            (- (exec a) (exec b))]
                 [`(,a * ,b)            (* (exec a) (exec b))]
                 [`(,a / ,b)            (/ (exec a) (exec b))]
                 [`(,a == ,b)           (convert-bool (equal? (exec a) (exec b)))]
-                [`(out ,name ,stuff)   (eval `(apply ,name '(,(exec stuff))) ns) (exec '(undefined))]
+                [`(,a : ,b)            (exec `(cons (,a ,b)))]
                 
-                [`(,a := ,b)            (set! area (cons `(,a ,b ,in-block) area))
-                                        (exec `,a)]
-                [`(,a ↦ ,b)            (set! area (cons `(,a ,b ,in-block) area))
-                                        (exec `,a)]
+                [`(,a := ,b)            (set! area (cons `(,a ,b ,block-level) area))
+                                        (exec `,a block-level)]
+                [`(,a ↦ ,b)            (set! area (cons `(,a ,b ,block-level) area))
+                                       (exec `,a block-level)]
                 
-                [`undefined               'e-undef]
+                [`undefined               'undefined]
                 [(? integer?)          code]
                 [(? string?)           code]
-                [(? symbol?)           (exec (car (cdr (lookup code area))))]
-                [`(,thing)             (exec thing)]
+                [(? symbol?)           (exec (second (lookup code area block-level)) block-level)]
+                [`(,thing)             (exec thing block-level)]
                 ['()                   (exec '(undefined))]))
-  
-  (define no-clojure (match res
-                       [`(,args ,body func) #f]
-                       [_ #t]))
-  
-  (if (and in-block no-clojure)
-      (set! area (filter (lambda (e)
-                           (displayln e)
-                           (eq? (caddr e) #f)) area))
-      #f)
   res)
 
 
