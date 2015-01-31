72e1a9d3377c833d5fc2459994a4ed6cec1e079b
christopherdumas <christopherdumas@me.com>
2015-01-31 09:46:11 -0800

    -38 LOC

--- alonzo.rkt
+++ alonzo.rkt
@@ -13,47 +13,6 @@
     [(eq? (second result) key) (lookup key (cdr list) block-level #t)]
     [else result]))
 
-(define (balanced? lst)
-  (letrec
-      ([count-open (lambda (l n)
-                     (cond
-                       [(empty? l) n]
-                       [(equal? (car l) #\() (count-open (cdr l) (add1 n))]
-                       [(equal? (car l) #\{) (count-open (cdr l) (add1 n))]
-                       [(equal? (car l) #\[) (count-open (cdr l) (add1 n))]
-                       [else (count-open (cdr l) n)]))]
-       [count-close (lambda (l n)
-                      (cond
-                        [(empty? l) n]
-                        [(equal? (car l) #\)) (count-close (cdr l) (add1 n))]
-                        [(equal? (car l) #\}) (count-close (cdr l) (add1 n))]
-                        [(equal? (car l) #\]) (count-close (cdr l) (add1 n))]
-                        [else (count-close (cdr l) n)]))])
-    (= (count-open (string->list lst) 0) (count-close (string->list lst) 0))))
-
-(define (read-full-lines file-name)
-  (define lines-lst '())
-  (define build-up "")
-  (define building? #f)
-  (for ([line (read-lines file-name)])
-    (cond
-      [(balanced? line) (if building?
-                            (set! build-up (string-append build-up line))
-                            ; else
-                            (set! lines-lst (append `(,line) lines-lst)))]
-      [(not (balanced? line)) (set! build-up (string-append build-up line))
-                              (set! building? #t)])
-    (if (balanced? build-up)
-        (begin
-          (set! lines-lst (append `(,build-up) lines-lst))
-          (set! build-up "")
-          (set! building? #f))
-        '()))
-  (cond
-    [(balanced? build-up) (set! lines-lst (append `(,build-up) lines-lst))
-                          (set! build-up "")]) ; one last time, for that last multiline statement.
-  lines-lst)
-
 (define (convert-bool bool)
   (if bool (exec '(true)) (exec '(false))))
 
@@ -72,7 +31,7 @@
 
 
 (define (exec code [block-level 0])
-  (define res (match code
+  (match code
                 [(list 'begin thing ... 'end)   (car (cdr (map exec thing)))]
                 [`(λ ,arg ,body)    (list `(,arg) `,body 'func)]
                 [`(fn ,args ,body)  (list `,args `,body 'func)]
@@ -87,8 +46,6 @@
                                        [k (car fn)])
                                    (set! area (cons (list k v (+ block-level 1)) area)))
                                  (last (map (lambda (e)
-                                              (display e)
-                                              (displayln (string-append " at " (number->string (+ block-level 1))))
                                               (exec e (+ block-level 1))) (second fn)))]
                 
                 [`(,a + ,b)            (+ (exec a) (exec b))]
@@ -109,7 +66,6 @@
                 [(? symbol?)           (exec (second (lookup code area block-level)) block-level)]
                 [`(,thing)             (exec thing block-level)]
                 ['()                   (exec '(undefined))]))
-  res)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -120,9 +76,7 @@
   (exec-repl))
 
 (define (exec-file file-name)
-  (define file (read-full-lines file-name))
-  (for ([line file])
-    (exec (read (open-input-string (string-append "(" line ")"))))))
+  (map exec (file->list file-name)))
 
 (exec-file "lib.alz")
 (exec-repl)
